8defc0dea3412bea6abb8cd476bc47781b100a58
Mahamat Naim <naimdevelopper@gmal.com>
2020-11-19 00:50:49 +0100

    notification finished

--- lib/constantes/model.dart
+++ lib/constantes/model.dart
@@ -1,9 +1,20 @@
 //model de l'information
+import 'package:website_university/constantes/string.dart';
+
 class Information {
   User user;
   String description;
   Document document;
   String date;
+  Information({this.user, this.description, this.document, this.date});
+}
+
+//model discution
+class Message {
+  User user;
+  String date;
+  String message;
+  Message({this.user, this.date, this.message});
 }
 
 //model de l'utilisateur
@@ -20,6 +31,47 @@ class Document {
   String description;
   String semestre;
   String universite;
+  String module;
+  String annee;
   String filiere;
   String urlPDF;
+  Document(
+      {this.titre,
+      this.universite,
+      this.filiere,
+      this.semestre,
+      this.description,
+      this.urlPDF,
+      this.annee,
+      this.module});
 }
+
+//////////////////////
+///////////////////
+Document d = Document(
+  annee: '2020-2021',
+  module: 'Programmation',
+  titre: 'Chapitre 2: langage Dart',
+  universite: 'Ibn Tofail',
+  semestre: 'S5',
+  filiere: 'SMI',
+  description:
+      'C\'est le deuxième chapitre concernant les essentiels de Flutter',
+);
+User user = User(nom: 'Naim Abdelkerim', admin: true, image: 'avatar.jpg');
+User user2 = User(nom: 'Ali', admin: false, image: 'avatar2.jpg');
+Information info = Information(
+    user: user,
+    document: d,
+    date: '14h:03 | 12-04-2020',
+    description:
+        'Nouveau fiche de cours hggggggghghg gkgfkkgf dddddddfff ggggggggggfgfgf');
+Message m = Message(
+    user: user, date: '04h:50 09/12/2020', message: 'Coucou tout le monde');
+Message m2 = Message(
+    user: user2,
+    date: '07h:50 19/12/2020',
+    message: 'Salut fhfhdn fdjfdfjfdjfdjf vhjdjfdfdjfjdb bmdjjdjfjdfjdf');
+
+List<Information> listInfo = [info, info, info, info];
+List listMessage = [m2, m, m2, m, m2];
--- lib/routes/notifications/notifications.dart
+++ lib/routes/notifications/notifications.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:website_university/constantes/couleur.dart';
+import 'package:website_university/constantes/model.dart';
 
 class Notifications extends StatefulWidget {
   @override
@@ -8,11 +9,153 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+  List<Information> listNotifications;
+
+  @override
+  void initState() {
+    listNotifications = listInfo;
+    super.initState();
+  }
+
+  double elevationCard = 2.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backColor,
+        //check
+        title: Icon(
+          Icons.notifications,
+          size: 35,
+          color: primary,
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       body: Container(
         color: backColor,
+        child: ListView.builder(
+            itemCount: listNotifications.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 8.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        userSection(index),
+                        contentSection(index),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            listNotifications[index].date,
+                            style:
+                                TextStyle(fontFamily: 'Ubuntu', fontSize: 10),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+
+  Container contentSection(int index) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            //                    <--- top side
+            color: backColor,
+
+            width: 1.0,
+          ),
+        ),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: [
+          Text(
+            listNotifications[index].description,
+            style: TextStyle(fontFamily: 'Ubuntu'),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          FlatButton.icon(
+            color: primary,
+            onPressed: () {},
+            icon: Icon(
+              Icons.article,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Afficher',
+              style: TextStyle(
+                  fontFamily: 'Didac', color: Colors.white, letterSpacing: 2),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container userSection(int index) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            //                    <--- top side
+            color: backColor,
+
+            width: 1.0,
+          ),
+        ),
+      ),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        alignment: WrapAlignment.spaceBetween,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: CircleAvatar(
+                  backgroundImage:
+                      AssetImage(listNotifications[index].user.image),
+                ),
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                listNotifications[index].user.nom,
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ],
+          ),
+          Icon(
+            Icons.star_purple500_outlined,
+            color: Colors.amber,
+            size: 20,
+          )
+        ],
       ),
     );
   }
